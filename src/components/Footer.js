31bc45f472d2f67b27a7cdd2b63b4da45bdd0be7
Aaryan-02 <102778679+Aaryan-02@users.noreply.github.com>
2023-07-04 19:47:09 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechSolutions</h1>
+        <p>@ll rights reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://youtube.com/6packprogrammer" target={'blank'}>Youtube</a>
+            <a href="https://instagram.com/6packprogrammer" target={'blank'}>Instagram</a>
+            <a href="https://github.com/6packprogrammer" target={'blank'}>Github</a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
